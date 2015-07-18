34fdf6615fd29531276e80473f5259414e117052
Todd Trimakas <todd.trimakas@gmail.com>
2015-07-18 17:47:22 +0000

    Added validations and cool email regex

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  def setup
+    @recipe = Recipe.create(name: "", summary: "Great chicken parm recipe",
+    description: "do all these things, and then cook it really quick")
+  end
+  
+  
+  test "valid recipe" do
+    assert @recipe.valid?
+  end 
+ 
+  test "summary must be present" do
+  end
+  
+  test "name should not be too long" do
+    
+  end
+  
+end  
